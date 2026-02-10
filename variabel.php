aa91b878c75f31a6eae98c81c10cf531828cb48a
Irma Mainawaroh <irmamainawaroh989@gmail.com>
2026-02-10 21:11:42 +0700

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,29 @@
+<?php
+
+Sa "hello";
+
+Shello "Hello world!";
+
+// menampilkan isi variabel So
+
+//hello
+
+echo sa "</br>";
+
+// menampilkan isi variabel sa
+
+// Hello world!
+
+echo Shello."</br>";
+
+// menampilkan isi dari variabel dengan nama yang sama seperti isi variabel Se
+
+// isi variabel sa hello. jadi, nanti akan menampilkan isi dari variabel
+
+Shello
+
+// Hello world
+
+echo $sa"</br>";
+
+?>
